039e4a979a7156ce3a2649030b78bea6d6973cfa
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-11 17:56:10 +0300

    The first commit Example_006

---
+++ Example_006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ivan")
+{
+    Console.WriteLine("Welcome lopuh");
+}
+else
+{
+    Console.Write("I am glad to see you, ");
+    Console.WriteLine(username);
+}
