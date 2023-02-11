1ac948185b01a6cb5c65bccec15977168250f759
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-11 16:12:30 +0300

    The first step Example_005

---
+++ Example_005/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(1, 10); //1,2,3 ... 9
+Console.Write("NumberA = ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("NumberB = ");
+Console.WriteLine(numberB);
+Console.Write("Result = ");
+Console.WriteLine(numberA+numberB);
